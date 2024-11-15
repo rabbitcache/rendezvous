194059106a9622eee22afdeb24167855a97597e1
Nicole <98623124+rabbitcache@users.noreply.github.com>
2024-11-15 14:54:36 -0600

    Interrupts working
    states 1-4 working

--- StringHandoffImpl.java
+++ StringHandoffImpl.java
@@ -8,7 +8,7 @@ public class StringHandoffImpl implements StringHandoff {
     private String bin;
 
     public StringHandoffImpl() {
-        bin = " ";
+        bin = null;
     }
 
     @Override
@@ -16,31 +16,17 @@ public class StringHandoffImpl implements StringHandoff {
         throws InterruptedException, TimedOutException, ShutdownException, IllegalStateException {
         long msEndTime = System.currentTimeMillis() + msTimeout;
         if (msTimeout == 0L) {
-            // Receiver first: drop off message, notify all
-            if(bin.equals(" ")){
-                bin = msg;
-                notifyAll();
-            }
-            // Passer First: drop off message and wait until receiver arrives
-            else {
-                bin = msg;
-                wait();
-            }
+            bin = msg;
+            notifyAll();
+            wait();
         }
         else {
             long msRemaining = msEndTime - System.currentTimeMillis();
             //if (msRemaining < 1L) throw TimedOutException;
             wait(msRemaining);
-            // Receiver first: drop off message, notify all
-            if(bin.equals(" ")){
-                bin = msg;
-                notifyAll();
-            }
-            // Passer First: drop off message and wait until receiver arrives
-            else {
-                bin = msg;
-                wait();
-            }
+            bin = msg;
+            notifyAll();
+            wait();
         }
     }
 
@@ -56,14 +42,15 @@ public class StringHandoffImpl implements StringHandoff {
         String msg = null;
         if (msTimeout == 0L) {
             // Passer first: just go and pick up message
-            if (! bin.equals(" ") ) {
+            if (bin != null) {
                 msg = bin;
+                notifyAll();
                 return msg;
             }
             // Receiver first: wait until passer arrives
             else {
                 notifyAll();
-                while (bin.equals(" ")) wait();
+                while (bin == null) wait();
                 msg = bin;
                 return msg;
             }
@@ -73,14 +60,15 @@ public class StringHandoffImpl implements StringHandoff {
             //if (msRemaining < 1L) return "unsucessful";
             wait(msRemaining);
             // Passer first: just go and pick up message
-            if (! bin.equals(" ") ) {
+            if (bin != null) {
                 msg = bin;
+                notifyAll();
                 return msg;
             }
             // Receiver first: wait until passer arrives
             else {
                 notifyAll();
-                while (bin.equals(" ")) wait();
+                while (bin == null) wait();
                 msg = bin;
                 return msg;
             }
